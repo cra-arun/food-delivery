ebf8d5ed6049d27fc4eea218cde4083052e183ac
CHINNA ROYYALA ARUN KUMAR <160038719+cra-arun@users.noreply.github.com>
2025-07-06 18:23:33 +0530

    Update firebase-admin.js

--- backend/firebase-admin.js
+++ backend/firebase-admin.js
@@ -1,13 +1,12 @@
-// backend/firebase-admin.js
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://food-delivery-405c8-default-rtdb.firebaseio.com"
+  databaseURL: process.env.FIREBASE_DB_URL
 });
 
 const db = admin.database();
 
-module.exports = { admin, db };
+module.exports = db;
